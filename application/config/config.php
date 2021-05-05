6874d15144d3aa73af09f3d82ffd8706b82319cf
ANIEANTONY <61555169+ANIEANTONY@users.noreply.github.com>
2021-05-05 17:39:48 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/foodproject/';
+$config['base_url'] = 'https://anieantony.github.io/foodcourt/localhost/foodproject/';
 
 /*
 |--------------------------------------------------------------------------
